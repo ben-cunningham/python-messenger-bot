a189e082bcff2f2fcc7f8e967ecadb0f75d83d88
ben-cunningham <bencunningham17@gmail.com>
2016-06-13 23:23:51 -0700

    Update release to 0.1.6

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name = 'fbmsgbot',
     packages = ['fbmsgbot'],
-    version = '0.1',
+    version = '0.1.6',
     description = 'Python bot for Facebook Messenger',
     author = 'Benjamin Cunningham & Austin Hendy',
     author_email = 'bencunningham17@gmail.com',
     url = 'https://github.com/ben-cunningham/pybot',
-    download_url = 'https://github.com/ben-cunningham/pybot/tarball/0.1',
+    download_url = 'https://github.com/ben-cunningham/pybot/tarball/0.1.6',
     classifiers = [],
 )
