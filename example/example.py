d3fdc3814294d4cf6d854caa03f78316968075c8
ben-cunningham <bencunningham17@gmail.com>
2016-08-27 17:10:11 -0700

    Update example with button

--- example/example.py
+++ example/example.py
@@ -7,6 +7,8 @@ sys.path.append("..")
 
 from fbmsgbot.bot import Bot
 from fbmsgbot.models.message import Message
+from fbmsgbot.models.template import Template
+from fbmsgbot.models.attachment import WebUrlButton, Element
 
 import json
 
@@ -20,8 +22,24 @@ def webhook():
 
     msgs = bot.messages_for_request(request)
     for m in msgs:
-    	msg = Message(m.sender, 'text', m.text)
+
+    	msg = None
+
+    	if m.text == 'button':
+    		buttons = []
+    		b = WebUrlButton('google', 'https://www.google.ca')
+    		buttons.append(b)
+    		elements = [Element('test', 'http://www.newton.ac.uk/files/covers/968361.jpg', 'test subtitle', buttons)]
+    		tmpl = Template('generic', elements=elements)
+
+    		payload = tmpl
+    		msg = Message(m.sender, 'template', payload)
+    	else:
+    		payload = m.text
+    		msg = Message(m.sender, 'text', payload)
+
     	response, error = bot.send_message(msg)
+    	print response
 
     	if error:
     		return 'Bad Request'
