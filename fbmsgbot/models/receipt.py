6e2b489625ef5cb33ba5d1c050e59bb2ac10a643
ben-cunningham <bencunningham17@gmail.com>
2016-09-10 19:29:31 -0700

    Add ReceiptElement

---
+++ fbmsgbot/models/receipt.py
@@ -0,0 +1,28 @@
+from attachment import Element
+
+class ReceiptElement(Element):
+
+    def __init__(self, quantity=None, price=None, 
+                       currency="", **kwargs):
+
+        self.kwargs = kwargs
+        super(ReceiptElement, self).__init__(**self.kwargs)
+        
+        self.quantity = quantity
+        self.price = price
+        self.currency = currency
+
+    def to_json(self):
+
+        print self.title
+        
+        payload = {
+            'title': self.title,
+            'subtitle': self.subtitle,
+            'quantity': self.quantity,
+            'price': self.price,
+            'currency': self.currency,
+            'image_url': self.image_url
+        }
+
+        return payload
